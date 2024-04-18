97fbc6d8fd94b269afc161659bb2ee686a7cca18
hwayoung lee <hylee13ee@gmail.com>
2024-04-18 11:21:04 +0900

    feat: 페이징처리

--- src/hooks/userFetchDocument.jsx
+++ src/hooks/userFetchDocument.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { db } from '@/firebase/firebase';
+import { DocumentData, doc, getDoc } from 'firebase/firestore';
+import React, { useCallback, useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 
-const userFetchDocument = () => {
-  return (
-    <div>userFetchDocument</div>
-  )
+const useFetchDocument = (collectionName, documentID) => {
+  const [document, setDocument] = useState<DocumentData | null>(null);
+
+  const getDocument = useCallback(async () => {
+    const docRef = doc(db, collectionName, documentID);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const obj = {
+        id: documentID,
+        ...docSnap.data()
+      }
+
+      setDocument(obj);
+    } else {
+      toast.error("Document not found");
+    }
+  }, [collectionName, documentID])
+
+  useEffect(() => {
+    getDocument();
+  }, [getDocument])
+
+  return { document }
 }
 
-export default userFetchDocument
+export default useFetchDocument
